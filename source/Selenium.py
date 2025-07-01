5057535144a8b02eb0c7b4fd55ca5a7760f0f3e4
Gali-Wang <q408960120@qq.com>
2025-07-02 01:01:53 +0800

    upload code

--- source/Selenium.py
+++ source/Selenium.py
@@ -3,13 +3,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import time
 
-option = Options()
-option.add_experimental_option("detach", True)
-#option.add_argument("--no-sandbox")
 
+def setBrowser():
+    option = Options()
+    option.add_experimental_option("detach", True)
+    #option.add_argument("--no-sandbox")
 
-browser = webdriver.Chrome(options=option)
+    browser = webdriver.Chrome(options=option)
+    return browser
 
+browser = setBrowser()
 browser.get("https://www.baidu.com")
 time.sleep(3)
 browser.maximize_window()
