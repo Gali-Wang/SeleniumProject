75f7af24c79818204195289df360206853706f2a
Gali-Wang <q408960120@qq.com>
2025-07-03 00:17:22 +0800

    upload code

--- source/Selenium.py
+++ source/Selenium.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -12,7 +13,25 @@ def setBrowser():
     browser = webdriver.Chrome(options=option)
     return browser
 
+
 browser = setBrowser()
 browser.get("https://www.baidu.com")
+
+
+
+'''
+browser.get_screenshot_as_file('../img/screenshot.png') #浏览器截图
 time.sleep(3)
-browser.maximize_window()
+browser.maximize_window() #最大化
+time.sleep(3)
+browser.close() #关闭该标签页
+time.sleep(2)
+browser.refresh() #刷新页面
+time.sleep(3)
+browser.quit() #关闭浏览器
+'''
+
+
+searchItem = browser.find_element(By.ID, "kw").send_keys("原神")
+browser.find_element(By.ID, "su").click()
+
